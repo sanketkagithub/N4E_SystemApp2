b125f7394100265d49cf8a42d6d97f4fbdb22681
Sanket <sanket.kpatankar@cytrion.com>
2018-04-20 17:58:05 +0530

    minor change alarm null check

--- app/src/main/java/com/example/a10580/n4e_systemapp/receivers/BootCompleteReceiver.java
+++ app/src/main/java/com/example/a10580/n4e_systemapp/receivers/BootCompleteReceiver.java
@@ -12,11 +12,13 @@ import com.example.a10580.n4e_systemapp.services.ReportAppInstallationStatusInte
 public class BootCompleteReceiver extends BroadcastReceiver {
     static final String ACTION = "android.intent.action.BOOT_COMPLETED";
 
+
+    //This method is called after switching on mobile
     @Override
     public void onReceive(Context context, Intent intent) {
-        // after BOOT_COMPLETED, start Service
+        // after starting of device, start Service
         if (intent.getAction().equals(ACTION)) {
-            restartReportingInstallationStatus(context);
+            keepReportingInstallationStatus(context);
             Log.i("bootCompleted", "SystemAppBooted");
 
         }
@@ -27,10 +29,14 @@ public class BootCompleteReceiver extends BroadcastReceiver {
 
 
 
-    void restartReportingInstallationStatus(Context context) {
+    //This method checks old and new N4E's installation status, if changes found,
+// reports to server using intent service.
+
+    void keepReportingInstallationStatus(Context context) {
         Intent ishintent = new Intent(context, ReportAppInstallationStatusIntentService.class);
         PendingIntent pintent = PendingIntent.getService(context, 0, ishintent, 0);
         AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        assert alarm != null;
         alarm.cancel(pintent);
         // alarm.setRepeating(AlarmManager.RTC_WAKEUP, 2, 2, pintent);
         alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, 2, 2, pintent);
